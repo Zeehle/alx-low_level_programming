974fece8ecf13800c243fcb9a80a1b0fc608052f
Zeehle <guguzinhlemabuza@gmail.com>
2023-06-26 19:06:47 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,6 +8,7 @@
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
+
 	i = 0;
 	d = 0;
 	n = 0;
@@ -20,18 +21,14 @@ int _atoi(char *s)
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-		{
 			++d;
-		}
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d %  2)
-			{
 				digit = -digit;
 				n = n * 10 + digit;
 				f = 1;
-			}
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
